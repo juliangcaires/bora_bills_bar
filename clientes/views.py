b115e9ad48c3715738d4449b24017c926a1ee07a
Julian Caires <juliancaires@hotmail.com>
2025-11-25 19:48:59 -0300

    Atualização meio da aula 25/11

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Cliente
 
-# Create your views here.
+def lista_clientes(request):
+    clientes = Cliente.objects.order_by('-pontos', 'nome')
+    return render(request, 'clientes/lista_clientes.html', 
+        {'clientes' : clientes})
